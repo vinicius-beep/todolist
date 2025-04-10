21d77a50cb047591a56dba05302bce515dc30cd2
Vini <viniciusdasilvadossantos4@gmail.com>
2025-04-10 19:14:34 -0300

    foi adicionados as funcionalidades de tarefas

--- src/models/models.php
+++ src/models/models.php
@@ -2,15 +2,31 @@
     // Retorna as tarefas da sessão ou inicia um array vazio
     function getTaskx(){
         // Digite o seu código aqui
+        return $_SESSION['tasks'] ?? [];
     }
 
     // Adiciona uma nova task no array
     function addNewTask($description){
         // Seu código aqui
+        $tasks = getTasks();
+        $tasks[] = [
+            'id' => count($tasks) + 1,
+            'description' => $description,
+            'completed' => false
+        ];
+
+        $_SESSION['tasks'] = $tasks;
     }
 
     // Marca uma task como concluida
     function ToggleTask($id){
         // Seu código aqui
+        $tasks = getTasks();
+        foreach($tasks as $index => $task){
+            if($task['id'] == $id){
+                $tasks[$index]['completed'] = !$task['completed'];
+            }
+        }
+        $_SESSION['tasks'] = $tasks;
     }
 ?>
